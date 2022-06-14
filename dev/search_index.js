26e99c3b31ab846371e85e44158353f3b42811b1
Documenter.jl <documenter@juliadocs.github.io>
2022-06-14 23:47:55 +0000

    build based on 89a2cf4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Dataverse","category":"page"},{"location":"#Dataverse","page":"Home","title":"Dataverse","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Dataverse.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Dataverse]","category":"page"}]
+}
