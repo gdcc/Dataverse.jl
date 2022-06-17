25dbdc3cdfa24b742a5b1ca3fd11b65b56960048
gaelforget <gforget@mit.edu>
2022-06-17 15:37:45 -0400

    use DataFrames, PrettyTables in notebook

--- docs/src/notebook.jl
+++ docs/src/notebook.jl
@@ -18,6 +18,7 @@ end
 begin
 	using Pkg; Pkg.activate()
 	using Dataverse, PlutoUI
+	using DataFrames, PrettyTables
 	"Packages ready"
 end
 
@@ -37,8 +38,7 @@ This package is about interfaces to the [Dataverse](https://dataverse.org) proje
 # ╔═╡ e90fc026-b134-4694-87db-b5fc8fd1dcb2
 begin
 	doi_select = @bind doi Select(["10.7910/DVN/EE3C40","10.7910/DVN/RNXA2A","10.7910/DVN/AVVGYX"])
-
-	md"""## APIs
+	md"""## `get_dataset`
 
 	Select doi : $(doi_select)
 		
@@ -47,6 +47,9 @@ end
 
 # ╔═╡ 306ced7d-005e-4e50-8295-d911403ff5fa
 DOI="doi:"*doi
+
+# ╔═╡ 061ea815-66ef-4e06-987f-fbd1f7e837d7
+md"""## `get_children`"""
 
 # ╔═╡ e448e0ce-4991-4a75-b611-570aa64439f3
 md"""## Appendix"""
@@ -60,23 +63,76 @@ end
 # ╔═╡ 2240395b-4112-4e8a-a233-26fde9dffbc4
 dataset = NativeApi.get_dataset(DOI)
 
-# ╔═╡ b15daa71-7ded-41d8-9dd0-904a07112950
-files_list = dataset.json()["data"]["latestVersion"]["files"]
-
-# ╔═╡ e7330644-dc09-453d-bd4e-7174fea43cfd
-files_list[1]["dataFile"]
-
 # ╔═╡ ae0a3032-73ad-482b-a020-822f9a212fe1
 tree = NativeApi.get_children("ECCOv4r2", children_types= ["datasets", "datafiles"])
+
+# ╔═╡ ecfc53da-38b4-478c-8559-41746d3cd8d0
+begin
+	nf=length(tree[1]["children"])
+	num_select = @bind num Select(1:nf)
+	md"""Select file : $(num_select)"""
+end
+
+# ╔═╡ d8ee1f2c-2017-492c-8ad1-190072756936
+tree[1]["children"][num]
+
+# ╔═╡ ec69221f-fc55-441f-8e83-e14f13b33088
+string.(keys(tree[num]["children"][1]))
+
+# ╔═╡ b2795505-9109-4320-9724-9ec2a00929eb
+function files_to_DataFrame(files)	
+	nf=length(files)
+	filename=[files[ff]["filename"] for ff in 1:nf]
+	pid=[files[ff]["pid"] for ff in 1:nf]
+	datafile_id=[files[ff]["datafile_id"] for ff in 1:nf]
+	DataFrame(filename=filename,pid=pid,datafile_id=datafile_id)
+end
+
+# ╔═╡ 5933462e-8d94-40ca-b7ef-cdcabb892444
+let
+	files=tree[num]["children"]
+	df=files_to_DataFrame(files)
+	@pt df
+end
+
+# ╔═╡ 893b7e8c-3adb-411e-a1c2-5917d98d5a14
+function files_list_to_DataFrame(files)	
+	nf=length(files)
+	filename=[files[ff]["dataFile"]["filename"] for ff in 1:nf]
+	filesize=[files[ff]["dataFile"]["filesize"] for ff in 1:nf]
+	pidURL=[files[ff]["dataFile"]["pidURL"] for ff in 1:nf]
+	DataFrame(filename=filename,filesize=filesize,pidURL=pidURL)
+end
+
+# ╔═╡ 52dab617-8f6b-4166-8aac-21b221ed9120
+begin
+	files_list = dataset.json()["data"]["latestVersion"]["files"]
+	df1=files_list_to_DataFrame(files_list)
+	@pt df1
+end
+
+# ╔═╡ 266d8126-fbe9-4c47-a66e-95120a3cbbd5
+files_list[1]
+
+# ╔═╡ b37d33a3-7c44-477a-a8cb-df9511e15ce3
+files_list[1]["dataFile"]
 
 # ╔═╡ Cell order:
 # ╟─52c81a38-0b73-4585-8833-40a834c12312
 # ╟─e90fc026-b134-4694-87db-b5fc8fd1dcb2
 # ╟─306ced7d-005e-4e50-8295-d911403ff5fa
-# ╠═2240395b-4112-4e8a-a233-26fde9dffbc4
-# ╠═e7330644-dc09-453d-bd4e-7174fea43cfd
-# ╠═b15daa71-7ded-41d8-9dd0-904a07112950
-# ╠═ae0a3032-73ad-482b-a020-822f9a212fe1
+# ╟─2240395b-4112-4e8a-a233-26fde9dffbc4
+# ╟─52dab617-8f6b-4166-8aac-21b221ed9120
+# ╟─266d8126-fbe9-4c47-a66e-95120a3cbbd5
+# ╟─b37d33a3-7c44-477a-a8cb-df9511e15ce3
+# ╟─061ea815-66ef-4e06-987f-fbd1f7e837d7
+# ╟─ecfc53da-38b4-478c-8559-41746d3cd8d0
+# ╟─5933462e-8d94-40ca-b7ef-cdcabb892444
+# ╟─d8ee1f2c-2017-492c-8ad1-190072756936
+# ╟─ec69221f-fc55-441f-8e83-e14f13b33088
+# ╟─ae0a3032-73ad-482b-a020-822f9a212fe1
 # ╟─e448e0ce-4991-4a75-b611-570aa64439f3
 # ╟─8e7742bc-ed2e-11ec-2bbe-adbcf21330e7
 # ╟─d216f95e-fc57-4b89-b796-be08b3f137d2
+# ╟─b2795505-9109-4320-9724-9ec2a00929eb
+# ╟─893b7e8c-3adb-411e-a1c2-5917d98d5a14
