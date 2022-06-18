c09bc04249c5b6784b5a92ec079b67d5dafb8695
gaelforget <gforget@mit.edu>
2022-06-18 15:01:55 -0400

    add functions + demo, docs, tests

--- src/Dataverse.jl
+++ src/Dataverse.jl
@@ -1,11 +1,10 @@
 module Dataverse
 
-include("downloads.jl")
 include("pyDataverse.jl")
-
-export get_from_dataverse, dataverse_lists, example_lists
 export pyDataverse
 
+include("downloads.jl")
+export get_from_dataverse, dataverse_lists, example_lists
 get_from_dataverse=downloads.get_from_dataverse
 dataverse_lists=downloads.dataverse_lists
 example_lists=downloads.example_lists
