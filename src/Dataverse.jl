47e6b116dff5c5c6b015920364a573bd7a538e58
gaelforget <gforget@mit.edu>
2023-04-30 13:52:01 -0400

    update notebook (see previous commit)

--- src/Dataverse.jl
+++ src/Dataverse.jl
@@ -1,10 +1,10 @@
 module Dataverse
 
-include("pyDataverse.jl")
-export pyDataverse
-
 include("restDataverse.jl")
 import Dataverse.restDataverse: file_list
+
+include("pyDataverse.jl")
+export pyDataverse
 
 include("downloads.jl")
 import Dataverse.downloads: file_download, url_list
