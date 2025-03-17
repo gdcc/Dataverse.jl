e6d327d9a12e70486d50be16d5f7ad5153f8626b
gaelforget <gforget@mit.edu>
2025-03-17 10:08:28 -0400

    rm unused ArgoData dep, partly reactivate pyDataverse testing

--- ext/DataversePythonCallExt.jl
+++ ext/DataversePythonCallExt.jl
@@ -1,12 +1,21 @@
 module DataversePythonCallExt
 
-using Dataverse
-import PythonCall, NetworkOptions
+using Dataverse, PythonCall
+#import NetworkOptions
 
-function Dataverse.pyDataverse_APIs(flag=true;do_install=false,base_url = "https://dataverse.harvard.edu/")
-    do_install ? error("pyDataverse_install is now available via the Conda extension.") : nothing
-    cafile=NetworkOptions.ca_roots_path()
-    ENV["SSL_CERT_FILE"] = cafile
+"""
+    Dataverse.pyDataverse_APIs
+
+```
+using Dataverse, PythonCall
+Dataverse.pyDataverse_install()
+(DataAccessApi,NativeApi)=Dataverse.pyDataverse_APIs(true)
+dataset = NativeApi.get_dataset("doi:10.7910/DVN/KBHLOD")
+```
+"""
+function Dataverse.pyDataverse_APIs(flag=true; base_url = "https://dataverse.harvard.edu/")
+#   cafile=NetworkOptions.ca_roots_path()
+#   ENV["SSL_CERT_FILE"] = cafile
     tmp=PythonCall.pyimport("pyDataverse")
     api=PythonCall.pyimport("pyDataverse.api")
     return api.DataAccessApi(base_url), api.NativeApi(base_url)
@@ -14,9 +23,3 @@ end
 
 end
 
-"""
-import Dataverse: pyDataverse_APIs
-(DataAccessApi,NativeApi)=pyDataverse_APIs(true)
-DOI = "doi:10.7910/DVN/KBHLOD"
-dataset = NativeApi.get_dataset(DOI)
-"""
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
-using Dataverse, CondaPkg, PythonCall, UUIDs, Test
+using Dataverse, PythonCall, UUIDs, Test
 
-do_py_test=false
-if do_py_test
+do_py_test=1
+if do_py_test>0
     Dataverse.pyDataverse_install()
-    Dataverse.pyDataverse_APIs()
+    (DataAccessApi,NativeApi)=Dataverse.pyDataverse_APIs()
+    @test typeof(DataAccessApi)==Py
+end
 
+if do_py_test>1
     tmp=pyDataverse.demo("download")
     @test isfile(tmp[1])
 
@@ -36,7 +39,5 @@ end
     Dataverse.downloads.Downloads.download(url,fil)
     Dataverse.unzip(fil,tempdir())
     @test ispath(joinpath(tempdir(),"ne_110m_admin_0_countries.shp"))
-end
-
 end
 
